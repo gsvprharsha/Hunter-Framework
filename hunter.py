51b408ab514b3594092df7e2e591f7dcc395f40f
Prharsha G <63843351+gsvprharsha@users.noreply.github.com>
2022-01-07 14:49:18 +0530

    Update hunter.py
    Added Network Scanner functionality to the tool

--- hunter.py
+++ hunter.py
@@ -19,9 +19,9 @@ print(termcolor.colored("██║  ██║╚██████╔╝██�
 print(termcolor.colored("╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚══╝   ╚═╝   ╚══════╝╚═╝  ╚═╝ Script V1.0",'red'))
 print(" ")
 print(termcolor.colored("\nNote: Run this tool as ROOT User as it needs superuser permissions.", 'yellow', attrs=['bold']))
-print(termcolor.colored("1] Port Scanner \t\t\t 2]SSH Bruteforce", 'green', attrs=['bold']))
-print(termcolor.colored("3] ARP Spoof \t\t\t\t 4]Backdoor", 'green', attrs=['bold']))
-print(termcolor.colored("5] Password Cracker\n", 'green', attrs=['bold']))
+print(termcolor.colored("1] Port Scanner \t\t\t 2] SSH Bruteforce", 'green', attrs=['bold']))
+print(termcolor.colored("3] ARP Spoof \t\t\t\t 4] Backdoor", 'green', attrs=['bold']))
+print(termcolor.colored("5] Password Cracker \t\t\t 6] Network Scanner\n", 'green', attrs=['bold']))
 print(termcolor.colored("[=] Press 0 to exit\n",'magenta', attrs=['bold']))
 option = input("Choose an Option: ")
 
@@ -31,10 +31,12 @@ if option == "0":
 elif option == "1":
     portscanner.lame_open_ports()
 elif option =="2":
-    os.system("/bin/python /home/legion/Desktop/hunter_script/main.py")
+    os.system("sudo python3 main.py")
 elif option =="3":
     arp_spoofer.lame_arp_spoofer()
 elif option =="4":
     manager.lame_server()
 elif option =="5":
     os.system("sudo python3 cracker-password.py")
+elif option =="6":
+    os.system("sudo python3 networkscanner.py")
